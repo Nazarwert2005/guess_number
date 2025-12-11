e5dfb666c17adf7e8a3dfb04d408f280206a4dc1
Nazar Podrucnyj <podrucnyjnazar@gmail.com>
2025-12-12 01:10:00 +0300

    optimize key logic

--- guess_number.py
+++ guess_number.py
@@ -1,17 +1,24 @@
+# Импорт функции получения случайных чисел
+# из модуля random.
 from random import randint
 
-secret_number = randint(1, 100)
-print('Отгадай число от 1 до 100')
+# Получаем случайное число в диапазоне от 1 до 100.
+number = randint(1, 100)
+print('Угадайте число от 1 до 100')
 
-user_number = 0
-try_count = 0
+while True:
+    # Получаем число от пользователя и сохраняем его в переменную.
+    guess = int(input('Введите число: '))
 
-while user_number != secret_number:
-    try_count += 1
-    user_number = int(input(f'{try_count}-я попытка: '))
-    if user_number > secret_number:
-        print('Много')
-    elif user_number < secret_number:
-        print('Мало')
-    else:
-        print(f'Правильно. Я загадал {secret_number}')
+    # Если число меньше загаданного...
+    if guess < number:
+        # ...выводим сообщение.
+        print('Ваше число меньше того, что загадано.')
+    elif guess > number:
+        # ...выводим сообщение.
+        print('Ваше число больше того, что загадано.')
+    elif guess == number:
+        # ...прерываем выполнение программы и...
+        break
+# ...выводим сообщение.
+print('Отличная интуиция! Вы угадали число :)')
